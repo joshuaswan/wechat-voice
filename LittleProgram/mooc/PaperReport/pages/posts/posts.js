a688239e5e95c1606a1599487a4b5d92a536b1b2
joshuaswan <joshuaswan@sina.com>
2018-02-28 04:09:48 +0800

    update update the demo of IMOOC

--- LittleProgram/mooc/PaperReport/pages/posts/posts.js
+++ LittleProgram/mooc/PaperReport/pages/posts/posts.js
@@ -5,20 +5,38 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    // date:"Nov 18 2017",
+    // title: "title in js"
   },
+
+  process: function(){
+    // var date = "Nov 18 2017";
+    // var date_ele = document.getElementsById
+  }
+  ,
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    console.log("onload");
+    var post_content1={
+      date:"Sep 18 2017",
+        title:"title 01",
+        post_img:"/images/post/crab.png",
+        content:"this is the first post content",
+        view_num:"112",
+        collect_num:"98",
+        author_img:"/images/avatar/1.png"
+    };
+    this.setDate(post_content1);
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
+    console.log("onReady");
   
   },
 
@@ -26,6 +44,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
+    console.log("onShow");
   
   },
 
@@ -33,6 +52,7 @@ Page({
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
+    console.log("onHide");
   
   },
 
@@ -40,6 +60,7 @@ Page({
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
+    console.log("onUnload");
   
   },
 
@@ -47,6 +68,7 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
+    console.log("onPullDownRefresh");
   
   },
 
@@ -54,6 +76,7 @@ Page({
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
+    console.log("onReachBottom");
   
   },
 
@@ -61,6 +84,7 @@ Page({
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
+    console.log("onShareAppMessage");
   
   }
 })
