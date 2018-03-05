6f90739c06c29b7bc8fada74bd91efc6e790af42
joshuaswan <joshuaswan@sina.com>
2018-03-05 18:11:17 +0800

    write the menu of the voice

---
+++ LittleProgram/voice/data/menu_list.js
@@ -0,0 +1,37 @@
+var menu_list= [
+  {
+    id: 'voiceSquare',
+    name: '声音广场',
+    pages: "/pages/voiceSquare/voiceSquare",
+    image: '/resource/images/logo.png'
+  }, {
+    id: 'dubRoom',
+    name: '配音室',
+    pages: "/pages/dubRoom/dubRoom",
+    image: '/resource/images/logo.png'
+  }, {
+    id: 'dubLesson',
+    name: '配音教室',
+    pages: "/pages/dubLesson/dubLesson",
+    image: '/resource/images/logo.png'
+  },{
+    id: 'practicingRoom',
+    name: '练习室',
+    pages: "/pages/practicingRoom/practicingRoom",
+    image: '/resource/images/logo.png'
+  },{
+    id: 'myVoice',
+    name: '我的声音',
+    pages: "/pages/myVoice/myVoice",
+    image: '/resource/images/logo.png'
+  },{
+    id: 'myMaterial',
+    name: '我的素材',
+    pages: "/pages/myMaterial/myMaterial",
+    image: '/resource/images/logo.png'
+  }
+]
+
+module.exports = {
+  local_menu_list: menu_list
+}
