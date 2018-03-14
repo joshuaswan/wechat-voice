90e076c63479310f56a70195d53a47081816a27b
joshuaswan <joshuaswan@sina.com>
2018-03-14 17:44:07 +0800

    create local voice project.

---
+++ LittleProgram/voice-local/pages/index/index.js
@@ -0,0 +1,181 @@
+//index.js
+//获取应用实例
+const app = getApp()
+var menuList = require('../../data/menu_list.js')
+var qcloud = require('../../vendor/wafer2-client-sdk/index')
+var config = require('../../config')
+var util = require('../../utils/util.js')
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    //初始化菜单数据
+    this.setData({
+      menuList: menuList.local_menu_list
+    });
+
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+    this.getUserInfo();
+    // this.login();
+    // 做登录开发的时候，如果你已经获取到了code，接下来获取session_key的时候。你需要将code传到你自己的服务器，然后在你自己的服务器请求session_key，而不是在小程序内部直接请求微信的url获取session_key。
+    // wx.login({
+    //   //获取code
+    //   success: function (res) {
+    //     var code = res.code; //返回code
+    //     console.log(code);
+    //     var appId = 'wx71aafc68f6c46172';
+    //     var secret = 'b1c36aeec83b253e988b3112cad06650';
+    //     wx.request({
+    //       url: 'https://api.weixin.qq.com/sns/jscode2session?appid=' + appId + '&secret=' + secret + '&js_code=' + code + '&grant_type=authorization_code',
+    //       data: {},
+    //       header: {
+    //         'content-type': 'json'
+    //       },
+    //       success: function (res) {
+    //         var openid = res.data.openid //返回openid
+    //         console.log('openid为' + openid);
+    //       }
+    //     })
+    //   }
+    // })
+  },
+
+  login: function () {
+    if (this.data.logged) return
+    
+    qcloud.setLoginUrl(config.loginUrl)
+
+    util.showBusy('正在登录')
+    var that = this
+
+    // 调用登录接口
+    qcloud.login({
+      success(result) {
+        if (result) {
+          util.showSuccess('登录成功')
+          that.setData({
+            userInfo: result,
+            logged: true
+          })
+        } else {
+          // 如果不是首次登录，不会返回用户信息，请求用户信息接口获取
+          qcloud.request({
+            url: config.service.requestUrl,
+            login: true,
+            success(result) {
+              util.showSuccess('登录成功')
+              that.setData({
+                userInfo: result.data.data,
+                logged: true
+              })
+            },
+
+            fail(error) {
+              util.showModel('请求失败', error)
+              console.log('request fail', error)
+            }
+          })
+        }
+      },
+
+      fail(error) {
+        util.showModel('登录失败', error)
+        console.log('登录失败', error)
+      }
+    })
+  },
+  getUserInfo: function (e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
